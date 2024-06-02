7fd246d831dd0b6be98156fa4df7566efc8bcd7c
Preet Pambhar <preetpambhar73530@gmail.com>
2024-06-02 00:35:03 -0400

    Initial Commit

---
+++ Car Hunt/Car_HuntApp.swift
@@ -0,0 +1,17 @@
+//
+//  Car_HuntApp.swift
+//  Car Hunt
+//
+//  Created by Preet Pambhar on 2024-06-02.
+//
+
+import SwiftUI
+
+@main
+struct Car_HuntApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
